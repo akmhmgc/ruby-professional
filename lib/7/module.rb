84d26940acf41d3c543ca440a71ed11527fa5485
akmhmgc <akimuhamaguchi@icloud.com>
2021-09-05 10:03:25 +0900

    モジュール関数について理解

---
+++ lib/7/module.rb
@@ -0,0 +1,26 @@
+module M
+  def foo
+    p "foo"
+  end
+  alias bar foo
+  #   fooをモジュール関数として定義（インスタンスではprivateメソッド）
+  module_function :foo
+end
+
+class C
+  include M
+
+  #   プライベートメソッドの呼び出し
+  def hello
+    foo
+  end
+end
+
+# 特異メソッドとして呼び出し
+M.foo
+
+# インスタンメソッドとして呼び出し
+c = C.new
+c.bar
+c.hello
+# c.foo fooはprivateメソッドなので呼び出せない
